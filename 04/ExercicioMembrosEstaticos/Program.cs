38b4c7404ee2ae087a9691f823609976ac28daac
Vitor Carraro <vitorwcarraro@gmail.com>
2025-05-16 15:20:54 -0300

    Delete Test

--- 04/ExercicioMembrosEstaticos/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using ExercicioMembrosEstaticos;
-
-Console.Write("Qual a cotação do dolar? ");
-double cotacaoDolar = double.Parse(Console.ReadLine());
-Console.Write("Quantos dólares você vai comprar? ");
-double quantidade = double.Parse(Console.ReadLine());
-Console.WriteLine("Valor a ser pago em reais = {0}", ConversorDeMoeda.DolarToReal(quantidade, cotacaoDolar));
--- 13/ExerciciosInterfaces/Servicies/IOnlinePaymentService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExerciciosInterfaces.Servicies
-{
-    internal interface IOnlinePaymentService
-    {
-        public double PaymentFee(double amount);
-        public double Interest(double amount, int month);
-    }
-}
