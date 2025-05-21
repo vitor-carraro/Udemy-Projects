44c1963b40f2e310312eb127f7cd6450301142d5
Vitor Carraro <vitorwcarraro@gmail.com>
2025-05-21 13:46:55 -0300

    Add ExercicioConjuntos and ExercicioDictionary projects

---
+++ Curso-CSharp/14/ExercicioConjuntos/Program.cs
@@ -0,0 +1,35 @@
+﻿
+HashSet<string> studentsA = new HashSet<string>();
+
+Console.Write("How many students for course A?");
+int N = int.Parse(Console.ReadLine());
+for (int i = 0; i < N; i++)
+{
+    studentsA.Add(Console.ReadLine());
+}
+
+HashSet<string> studentsB = new HashSet<string>();
+
+Console.Write("How many students for course B?");
+N = int.Parse(Console.ReadLine());
+for (int i = 0; i < N; i++)
+{
+    studentsB.Add(Console.ReadLine());
+}
+
+HashSet<string> studentsC = new HashSet<string>();
+
+Console.Write("How many students for course C?");
+N = int.Parse(Console.ReadLine());
+for (int i = 0; i < N; i++)
+{
+    studentsC.Add(Console.ReadLine());
+}
+
+HashSet<string> studentsAll = new HashSet<string>();
+
+studentsAll.UnionWith(studentsA);
+studentsAll.UnionWith(studentsB);
+studentsAll.UnionWith(studentsC);
+
+Console.WriteLine($"Total students: {studentsAll.Count}");
---
+++ Curso-CSharp/14/ExercicioDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+    using (StreamReader sr = new StreamReader(File.OpenRead(path)))
+    {
+        Dictionary<string, int> votes = new Dictionary<string, int>();
+        
+        while (!sr.EndOfStream)
+        {
+            string[] lineVoteRecord = sr.ReadLine().Split(',');
+            int numberOfVotes = int.Parse(lineVoteRecord[1]);
+
+            if (!votes.TryAdd(lineVoteRecord[0], numberOfVotes))
+            {
+                votes[lineVoteRecord[0]] += numberOfVotes;
+            }
+        }
+
+        foreach(var item in votes)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine($"An file error ocurred: {e.Message}");
+}
