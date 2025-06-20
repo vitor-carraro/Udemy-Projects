d9796f4dbc2b7f7e64becbaec308784b3457ed9a
Vitor Carraro <vitorwcarraro@gmail.com>
2025-06-20 16:51:50 -0300

    Add initial project structure and employee processing logic
    Created `ExercicioLambda.csproj` and `ExercicioLambda.sln` for a .NET 8.0 console application. Implemented `Program.cs` to read employee data from a file, filter by salary, and calculate sums for specific names. Added `Employee.cs` to define the employee model with necessary properties and constructor.

---
+++ Curso-CSharp/15/ExercicioLambda/Program.cs
@@ -0,0 +1,47 @@
+﻿using ExercicioLambda.Entities;
+using System.Globalization;
+
+Console.Write("Enter full file path: ");
+string filePath = Console.ReadLine();
+
+Console.Write("Enter salary: ");
+double salary = double.Parse(Console.ReadLine());
+
+List<Employee> employees = new List<Employee>();
+
+using (StreamReader sr = File.OpenText(filePath))
+{
+    while (!sr.EndOfStream)
+    {
+        string[] fields = sr.ReadLine().Split(',');
+        double employeeSalary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+
+        employees.Add(new Employee(fields[0], fields[1], employeeSalary));
+    }
+}
+
+Console.WriteLine($"Email of people whose salary is more than {salary}:");
+employees.Where(e => e.Salary > salary)
+         .Select(e => e.Email)
+         .OrderBy(e => e)
+         .ToList()
+         .ForEach(Console.WriteLine);
+
+Console.Write("Sum of salary of people whose name starts with 'M': ");
+var sum = employees.Where(e => e.Name.StartsWith('M'))
+         .Select(e => e.Salary)
+         .DefaultIfEmpty(0.0)
+         .Sum()
+         .ToString("F2");
+Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+
+
+
