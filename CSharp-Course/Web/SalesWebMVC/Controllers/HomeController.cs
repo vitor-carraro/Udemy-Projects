c04735422fd8f5ad52f779dbb9a0959c2cefff64
Vitor Carraro <vitorwcarraro@gmail.com>
2025-06-25 16:49:11 -0300

    First Modification of Project
    Defined .Net Core 1.2 Framework Project

--- CSharp-Course/Web/SalesWebMVC/Controllers/HomeController.cs
+++ CSharp-Course/Web/SalesWebMVC/Controllers/HomeController.cs
@@ -1,20 +1,32 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMVC.Models;
+using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 
-namespace SalesWebMVC.Controllers
+namespace SalesWebMvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
             return View();
         }
 
